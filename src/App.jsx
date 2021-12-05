2bfb3c4e095eb7ee0d534070b21e68ffb2a87f4e
Alina <alinakassym@gmail.com>
2021-12-06 00:36:50 +0600

    change a -> Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { About } from "./pages/AboutPage";
+import { Blog } from "./pages/BlogPage";
+import { Home } from "./pages/HomePage";
+import { NotFound } from "./pages/NotFoundPage";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <>
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/blog">Blog</Link>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    </>
   );
 }
 
