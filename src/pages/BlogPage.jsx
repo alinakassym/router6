2ba23e57d11224b32fc4e82a75590cfc57c9d099
Alina <alinakassym@gmail.com>
2021-12-06 00:10:06 +0600

    add  pages

---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,10 @@
+const Blog = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>This is demo website about React-router-dom library</p>
+        </div>
+    )
+}
+
+export {Blog}
