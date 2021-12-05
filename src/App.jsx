b08e12da07f5a222cf0258e56ba23681a4d5df6b
Alina <alinakassym@gmail.com>
2021-12-06 02:10:09 +0600

    add CreatePostPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { About } from "./pages/AboutPage";
 import { Blog } from "./pages/BlogPage";
+import { CreatePost } from "./pages/CreatePostPage";
 import { Home } from "./pages/HomePage";
 import { NotFound } from "./pages/NotFoundPage";
 import { Post } from "./pages/PostPage";
@@ -16,6 +17,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="posts" element={<Blog />} />
         <Route path="posts/:id" element={<Post />} />
+        <Route path="posts/new" element={<CreatePost />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
---
+++ src/pages/CreatePostPage.jsx
@@ -0,0 +1,9 @@
+const CreatePost = () => {
+    return (
+        <section>
+            <h1>Create new post</h1>
+        </section>
+    )
+}
+
+export {CreatePost}
