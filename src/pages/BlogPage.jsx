0de3f7f7708939c64c966cc92c32c0eea67711ba
Alina <alinakassym@gmail.com>
2021-12-07 23:35:49 +0600

    BlogPage: add filter latest

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -7,14 +7,23 @@ const Blog = () => {
 
   const postQuery = searchParams.get('post') || '';
 
+  const latest = searchParams.has('latest');
+
+  const startsFrom = latest ? 80 : 1;
+
   const handleSubmit = e => {
     e.preventDefault();
 
     const form = e.target;
 
     const query = form.search.value;
+    const isLatest = form.latest.checked;
 
-    setSearchParams({post: query});
+    const params = {}
+
+    if (query.length) params.post = query;
+    if (isLatest) params.latest = true;
+    setSearchParams(params);
   };
 
   useEffect(() => {
@@ -26,15 +35,20 @@ const Blog = () => {
     <section>
       <h1>Blog</h1>
 
-      <form autoComplete="off" onSubmit={handleSubmit} style={{marginBottom: "20px", display: "flex", flexDirection: "row"}}>
+      <form autoComplete="off" onSubmit={handleSubmit} style={{marginBottom: "20px", display: "flex", flexDirection: "row", alignItems: "center"}}>
         <input type="search" name="search" />
         <input type="submit" value="Search" />
+
+        <label style={{marginLeft: "20px", display: "flex", alignItems: "center"}}>
+          Latest:
+          <input type="checkbox" name="latest" style={{marginBottom: 2, marginLeft: 8}} />
+        </label>
       </form>
 
       <Link to="/posts/new">Add new posts</Link>
       <p>This is demo website about React-router-dom library</p>
       <ul>
-        {posts.filter(post => post.title.includes(postQuery)).map(post => (
+        {posts.filter(post => post.title.includes(postQuery) && post.id >= startsFrom).map(post => (
           <Link key={post.id} to={`/posts/${post.id}`}>
             <li>{post.title}</li>
           </Link>
