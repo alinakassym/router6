8b9593437f734c98aa154d617c38d8c1ed5be7e1
Alina <alinakassym@gmail.com>
2021-12-09 17:26:30 +0600

    nested routing

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="about/*" element={<About />}>
+            <Route path="contacts" element={<p>Contacts</p>}></Route>
+            <Route path="team" element={<p>Team</p>}></Route>
+          </Route>
           <Route path="about-us" element={<Navigate to={'/about'} replace />} />
           <Route path="posts" element={<Blog />} />
           <Route path="posts/:id" element={<Post />} />
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,23 @@
+import { Link, Outlet } from "react-router-dom";
+
 const About = () => {
-    return (
-        <section>
-            <h1>About us</h1>
-            <p>This is demo website about React-router-dom library</p>
-        </section>
-    )
+  return (
+    <section>
+      <h1>About us</h1>
+      <p>This is demo website about React-router-dom library</p>
+
+      <ul>
+        <li><Link to="contacts">Contacts</Link></li>
+        <li><Link to="team">Team</Link></li>
+      </ul>
+
+      <Outlet />
+      {/* <Routes>
+        <Route path="contacts" element={<p>Contacts</p>}></Route>
+        <Route path="team" element={<p>Team</p>}></Route>
+      </Routes> */}
+    </section>
+  )
 }
 
-export {About}
+export { About }
